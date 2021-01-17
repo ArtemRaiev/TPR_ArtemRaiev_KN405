3959adc62313fea84e0daa667ddebca6ec2d8988
ArtemRaiev <artem.raiev.kn.2017@lpnu.ua>
2021-01-17 22:18:41 +0200

    done

---
+++ lab_5/js/main.js
@@ -0,0 +1,330 @@
+/**
+* Рішення задачі лінійного програмування
+* Табличний Симплекс метод
+*/
+
+$(document).ready(function(){
+	// Задаємо глобальну змінну
+	max_x = 2;
+	// Рахуємо максимальну кількість Іксів
+	$('#ogranichenie_block .ogranichenie').each(function(){
+		var input_num = $(this).find('input').length -1;
+		if (input_num>max_x)
+			max_x = input_num;
+	})
+	// Показуємо обмеження 
+	//(зроблено для плавної появи нових обмежень)
+	$('.ogranichenie').show();
+	
+})
+	// Показуємо або приховуємо приклад
+	$('#info a.example').live('click', function(){
+		if ($('#info .left').is(':hidden')){
+			$(this).text('Сховати приклад ▲');
+			$('#info .left').slideDown(200);
+		}else{
+			$('#info .left').slideUp(200);
+			$(this).text('Показати приклад ▼');
+		}
+		return false;
+	})
+	// Добавляємо Х
+	$('a.add_x').live('click', function(){
+		// Обмежили кількість Іксів до 7, інакше вони не поміщаються в рядок і виглядають негарно
+		if ($(this).parents('.virazhenie').find('input').length < 7){ // Обмеження можна зняти або змінити
+			if ($(this).parents('.uravnenie').length){
+				var count_x = $(this).parents('.virazhenie').find('input').length +1;
+				if (count_x > max_x)
+					max_x = count_x;
+			}else{
+				var count_x = $(this).parents('.virazhenie').find('input').length;
+				if (count_x > max_x)
+					max_x = count_x;		
+				
+			}
+			$('.virazhenie').children('.left_side').append('+<input type="text" value ="0" />X'+count_x);
+		}
+		return false;
+	})
+	
+	// Добавляємо обмеження
+	$('.ogranichenie_add a').live('click', function(){
+		var html_inputs = '<input type="text" value ="0" />X1';
+		for (var q = 2; q<=max_x;q++)
+			html_inputs += '+<input type="text" value ="0" />X'+q;
+		var html_code = '<div class="ogranichenie virazhenie"><span class="left_side">'+html_inputs+'</span><span class="right_side"><a href="#" class="add_x">+</a><select><option value="1">≤</option><option value="-1">≥</option></select><input type="text" value ="0" /></span></div>';		
+		$('#ogranichenie_block').append(html_code);
+		$('#ogranichenie_block .ogranichenie:hidden').slideDown(200);
+		
+		return false;
+	})	
+	// Почнемо рахувати
+	$('.submit a').live('click', function(){
+	$('#result').html(' '); // Очищаємо поле результатів
+		var matrix = new Array();
+	//	var count_ogr = $('#ogranichenie_block .ogranichenie').length;
+		matrix = new Array();
+		var i = 0;
+	/*################## Крок 0 ##################*/	
+	// Перебираємо всі обмеження
+		$('#ogranichenie_block .ogranichenie').each(function(){
+			matrix[i] = new Array();
+			for (var j = 0; j < max_x + 1; j++) {
+				if ($(this).find('input').eq(j).length && $(this).find('input').eq(j).val() ){
+					var inp_val = $(this).find('input').eq(j).val() * $(this).find('select').val();
+				}else{
+					var inp_val = 0;
+				}
+				matrix[i][j] = inp_val; // Матриця вихідних значень
+			}
+			i++;
+		})
+	// Масив індексів по горизонталі
+		horisont_x = new Array();
+		for (i=0; i< max_x + 1; i++){
+			horisont_x[i] = i;
+		}
+	// Масив індексів по вертикалі
+		vertical_x = new Array();
+		for (i=0; i< $('#ogranichenie_block .ogranichenie').length; i++){
+			vertical_x[i] = i + max_x;
+		}		
+	// Матриця вільних членів	
+		var free = new Array();
+		for (var k=0; k < matrix.length; k++){
+			free[k] = matrix[k][max_x];
+		}
+		free[matrix.length] = 0;
+
+	// Останній рядок сама функція
+		Fun = new Array();
+		for (var j = 0; j < matrix[0].length; j++) {
+			if ($('.uravnenie .left_side').find('input').eq(j).length){
+				var inp_val = $('.uravnenie .left_side').find('input').eq(j).val() * $('.uravnenie select').val();
+			}else{
+				var inp_val = 0;
+			}
+			Fun[j] = inp_val; // Матриця вихідних значень
+		}
+	// Додамо її в основну матрицю
+		matrix.push(Fun); 
+	
+	// Чи є негативні елементи в матриці вільних членів?
+	if (minelm(free) < 0){ 
+		iteration = 0; // лічильник ітерацій, для захисту від зависання
+		step1(); // Переходимо до кроку 1
+	}
+	// Чи є негативні елементи в коефіцієнт функції (останній рядок)?
+	if (minelm(matrix[matrix.length-1], false, true) < 0){
+		iteration = 0; // лічильник ітерацій, для захисту від зависання
+		step2(); // Переходимо до кроку 2
+	}	
+	print_table(matrix); // Відображаємо підсумкову таблицю
+	results(); // Відображаємо результати в зрозумілому вигляді
+	
+/*################## Крок 1 ##################*/
+function step1(){
+		iteration++;
+		// знаходимо провідний рядок
+		var min_k_num = minelm(free, true, true);
+		
+		// знаходимо провідний стовпець		
+		var min_k1 = minelm(free)
+		if (minelm(matrix[min_k_num]) < 0){
+			var min_k1_num = minelm(matrix[min_k_num], true, true);
+		}else{
+			alert('Умови завдання несумісні і рішень у неї немає');
+			return false;
+		}
+		// Друкуємо таблицю і виділяємо на ній провідні рядок і стовпець
+		print_table(matrix, min_k_num, min_k1_num); 
+		// Оновлюємо індекси елементів по горизонталі і вертикалі
+		tmp = horisont_x[min_k1_num];
+		horisont_x[min_k1_num] = vertical_x[min_k_num];
+		vertical_x[min_k_num] = tmp;
+		
+
+	// Заміна	
+		update_matrix(min_k_num, min_k1_num);
+	// матриця вільних членів
+		for (var k=0; k < matrix.length; k++){
+			free[k] = matrix[k][max_x];
+		}
+		
+
+		if (minelm(free, false, true) < 0 && iteration < 10) // чи потрібно ще разок пройти другий крок?
+		if (confirm("продовжуємо Крок 1_"+iteration+" ?")) 
+			step1();
+	
+}
+
+
+/*################## Крок 2 ##################*/	
+function step2(){
+		iteration++;
+		// знаходимо провідний стовпець
+		var min_col_num = minelm(matrix[matrix.length-1], true, true);
+		
+		// знаходимо провідний рядок
+		var cols_count = matrix[0].length -1;
+		var min_row_num = 999;
+		// емпіричний коефіцієнт
+		var min_row = 9999; 
+		var tmp = 0;
+		for (i = 0; i< matrix.length-1; i++){
+			tmp = free[i]/matrix[i][min_col_num];
+			if (tmp < min_row && tmp>=0){
+				min_row_num = i;
+				min_row = tmp;
+			}
+		}
+		
+		min_k1_num = min_col_num;
+		min_k_num = min_row_num;
+		// Друкуємо таблицю і виділяємо на ній провідні рядок і стовпець
+		print_table(matrix, min_k_num, min_k1_num);
+		// Оновлюємо індекси елементів по горизонталі і вертикалі
+		tmp = horisont_x[min_k1_num];
+		horisont_x[min_k1_num] = vertical_x[min_k_num];
+		vertical_x[min_k_num] = tmp;
+		// Якщо ми не знайшли провідний рядок (999 - це наш емпіричний коефіцієнт)
+		if (min_row_num == 999){
+			alert('функція в області допустимих рішень задачі не обмежена');
+			return false;
+		}
+
+	// Заміна	
+		update_matrix(min_k_num, min_k1_num);
+	// матриця вільних членів
+		for (var k=0; k < matrix.length; k++){
+			free[k] = matrix[k][max_x];
+		}
+		
+	// чи потрібно ще раз пройти 2 крок?	
+		if (minelm(matrix[matrix.length-1], false, true) < 0 && iteration < 10) 
+	// рекурсія		
+		if (confirm("продовжуємо Крок 2_"+iteration+" ?")) 
+			step2();
+
+}
+// Функція заміни (відновлення матриці)
+function update_matrix(min_k_num, min_k1_num){
+
+		var matrix1 = new Array();
+	
+		for (i = 0; i< matrix.length; i++){
+			matrix1[i] = new Array()
+			for (j = 0; j< matrix[0].length; j++){
+				if (i == min_k_num && j ==min_k1_num){
+					matrix1[i][j] = 1/matrix[i][j];
+				}else{
+					if (i == min_k_num){
+						matrix1[i][j] = matrix[i][j] * 1/matrix[min_k_num][min_k1_num];
+					}else{
+						if (j == min_k1_num){
+							matrix1[i][j] = -matrix[i][j] * 1/matrix[min_k_num][min_k1_num];
+						}else{
+							matrix1[i][j] = matrix[i][j] - matrix[i][min_k1_num]*matrix[min_k_num][j]/matrix[min_k_num][min_k1_num];
+						}
+					}
+			
+				}
+				matrix1[i][j] = Math.round(matrix1[i][j]*1000)/1000;
+			}
+		}
+		matrix = matrix1;
+
+	return false;
+
+}
+
+
+	// Виводимо результати в зрозумілому вигляді
+	function results(){
+		var nulls = '';
+		// Ікси, рівні нулю
+		for (i = 0; i< horisont_x.length-1;i++){
+			if (horisont_x[i]<max_x)
+				nulls +=('X'+(horisont_x[i]+1)+'=');
+		}
+		nulls +='0 <br /><br />';
+		var vars ='';
+		// Ікси, відмінні від нуля
+		for (i = 0; i< vertical_x.length;i++){
+			if (vertical_x[i]<max_x)
+				vars += 'X'+(vertical_x[i]+1)+'='+matrix[i][max_x]+'<br />';
+		}
+		var main_result = '';
+		// Мінімум (максимум) функції
+		if ($('.uravnenie select').val() > 0)
+			main_result = 'min F ='+matrix[matrix.length-1][max_x]*(-1);
+		else
+			main_result = 'max F ='+matrix[matrix.length-1][max_x];
+			
+		$('#result').append(nulls+vars+'<br />'+main_result);
+	}
+	return false;
+})
+	
+	
+	// Вивід таблиці.
+	function print_table(arr, row, col){
+		var max_i = arr.length;
+		var max_j = arr[0].length;
+		var html_table = '';
+		var html_head = '<th></th>';
+		// Заголовки
+		for (var j = 0; j < max_j-1; j++) {
+			html_head +='<th>X'+(horisont_x[j]+1)+'</th>'
+		}	
+		html_head +='<th>Своб.члены</th>'
+		html_head ='<thead><tr>'+html_head+'</tr></thead>';
+		// Елементи
+		for (var i = 0; i < max_i; i++) {
+			html_table +='<tr>';
+			if (!(i == max_i-1)){
+				html_table +='<th>X'+(vertical_x[i]+1)+'</th>';
+			}else{
+				html_table +='<th>F</th>';
+			}
+			
+			for (var j = 0; j < max_j; j++) {
+				html_table +='<td>'+arr[i][j]+'</td>'
+			}
+			html_table +='</tr>';
+		}
+	
+		$('#result').append('<table>'+html_head+html_table+'</table>');
+		// Виділяємо колонку, якщо вказана
+		if (col !== undefined)
+			$('table:last tr').each(function(){
+				$(this).children('td').eq(col).addClass('selected');
+			})
+		// Виділяємо рядок, якщо вказана
+		if (row !== undefined)
+			$('table:last tr').eq(row+1).addClass('selected');
+	}
+// Пошук мінімального елемента
+function minelm(v, dispnum, not_last){ 
+    var m= v[0];
+	var num= 0;
+	var len=0;
+	// якщо not_last, то останній елемент не враховуємо в масиві
+	if (not_last){
+		len = v.length-2;
+	}else{
+		len = v.length-1;
+	}
+    for (var i=1; i <= len; i++){ 
+		if (v[i] < m ){
+			m= v[i];
+			num = i
+		}
+    }
+	// Виводимо номер мінімального
+	if (dispnum){
+		return num
+	}else{ // або значення
+		return m
+	}
+}
